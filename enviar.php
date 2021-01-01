8e14fcb449e398fe5c0f1e8e552b950c407c2dfd
raulvallediaz <64618982+raulvallediaz@users.noreply.github.com>
2021-01-01 22:30:10 +0100

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,17 +1,12 @@
- <?php
-//Recibimos los campos del formulario
+<?php
    $nombre= $_POST["nombre"];
    $email= $_POST["email"];
    $telefono= $_POST["telefono"];
    $mensaje=$_POST["mensaje"];
-   $destinatario="raulvallediaz@gmail.com";
-   $asunto="Desde la web";
+   $destinatario= "raulvallediaz@gmail.com";
 
+   $contenido= "Nombre: " .$nombre . "\nEmail: " .$email . "\nTelefono: " .$telefono ."\nMensaje: " . $mensaje;
 
-//Acomodamos todo para darle orden al cuerpo del mensaje
-//$texto_mensaje="El usuario ".$nombre." dijo: \n".$mensaje." \n
-//Puedes contactarlo a su correo: ".$correo;
-
-mail($destinatario,$asunto,$texto_mensaje);
-echo "Su mensaje se ha enviado! Nos pondremos en contacto contigo.".$nombre;
+   mail($destinatario,$asunto,$contenido);
+   echo "Su mensaje se ha enviado! Nos pondremos en contacto contigo.".$nombre;
 ?>
