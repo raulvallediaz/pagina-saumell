fedbfad12ed6331e71e267e3d34e3018e6fd09a4
raulvallediaz <64618982+raulvallediaz@users.noreply.github.com>
2021-01-01 20:39:10 +0100

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -6,9 +6,9 @@ $asunto=$_POST['asunto'];
 $mensaje=$_POST['mensaje'];
 
 //Acomodamos todo para darle orden al cuerpo del mensaje
-$texto_mensaje="El usuario ".$nombre." dijo: \n".$mensaje." \n
-Puedes contactarlo a su correo: ".$correo;
+//$texto_mensaje="El usuario ".$nombre." dijo: \n".$mensaje." \n
+//Puedes contactarlo a su correo: ".$correo;
 
-mail("raulvalledia@gmail.com",$asunto,$texto_mensaje);
-echo "Su mensaje se ha enviado! Nos pondremos en contacto contigo.";
+//mail("raulvalledia@gmail.com",$asunto,$texto_mensaje);
+echo "Su mensaje se ha enviado! Nos pondremos en contacto contigo.".$nombre;
 ?>
