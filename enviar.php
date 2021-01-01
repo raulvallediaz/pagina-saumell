a0ebf69caa861b0e7cb7810383e6048630b9c12b
raulvallediaz <64618982+raulvallediaz@users.noreply.github.com>
2021-01-01 16:02:19 +0100

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -2,9 +2,9 @@
     $destino= "raulvallediaz@gmail.com";
     $nombre= $_POST["nombre"];
     $correo= $_POST["correo"];
-    $telefono= $_POST["telefon"];
+    $telefono= $_POST["telefono"];
     $mensaje= $_POST["mensaje"];
     $asunto= "Contacto web Aresa";
     $contenido= "Nombre: " .$nombre ."\nCorreo: " .$correo ."\nTeléfono:" .$telefono ."\nMensaje:" .$mensaje;
-    mail($destino,$contenido);
+    mail($destino,$asunto,$contenido);
 ?>
